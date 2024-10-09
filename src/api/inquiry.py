874d9adba1c39689f4d94f22a8da3e86593adc97
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2024-10-09 19:34:03 +0900

    ace_it project inquiry function commit

--- src/api/inquiry.py
+++ src/api/inquiry.py
@@ -1,25 +1,44 @@
-from fastapi import Depends, APIRouter
-from fastapi_mail import ConnectionConfig
+from fastapi import Depends, APIRouter, BackgroundTasks
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema
 
-from schema.request import CreateInquiryRequest
+from schema.request import CreateInquiryRequest, EmailSchema
 
 router = APIRouter(prefix="/inquiry")
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="<EMAIL>",
-    MAIL_PASSWORD="<PASSWORD>",
-    MAIL_FROM="<EMAIL>",
+    MAIL_USERNAME="ajffhsk2248@naver.com", # 보내는 사람 이메일
+    MAIL_PASSWORD="hjsong059718**",
+    MAIL_FROM="ajffhsk2248@naver.com",
     MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_SERVER="smtp.naver.com",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True
 )
 
-# @router.post("")
-# def send_inquiry(
-#         request: CreateInquiryRequest
-# ):
+@router.post("")
+def send_inquiry(
+        email: EmailSchema,
+        background_tasks: BackgroundTasks
+):
+
+    body = (
+        f"문의 작성자: {email.message.inquiry_writer}\n"
+        f"작성자 이메일: {email.message.inquiry_writer_email or '없음'}\n"
+        f"작성자 연락처: {email.message.inquiry_writer_contact_number or '없음'}\n"
+        f"문의 내용: {email.message.inquiry_details or '없음'}"
+    )
+
+    message = MessageSchema(
+        subject=email.subject,
+        recipients=[email.email],
+        body=body,
+        subtype='plain'
+    )
+
+    fm = FastMail(conf)
+    background_tasks.add_task(fm.send_message, message)
+    return {"message": "email has been sent"}
 
 # @router.get("", status_code=200)
 # def get_inquiry(
--- src/schema/request.py
+++ src/schema/request.py
@@ -1,7 +1,7 @@
 from datetime import date
 from typing import Optional, Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class CreateInfoRequest(BaseModel):
@@ -36,7 +36,12 @@ class CreateBusinessAreaRequest(BaseModel):
 
 # 받은 데이터 특정 이메일로 발송 필요!
 class CreateInquiryRequest(BaseModel):
-    inquiry_writer: str
-    inquiry_writer_email: Optional[str] = None
-    inquiry_writer_contack_number: Optional[str] = None
-    inquiry_details: Optional[str] = None
+    inquiry_writer: str # 문의 작성자
+    inquiry_writer_email: Optional[str] = None # 문의 작성자 이메일 주소
+    inquiry_writer_contact_number: Optional[str] = None # 문의 작성자 연락처
+    inquiry_details: Optional[str] = None # 문의 내용
+
+class EmailSchema(BaseModel):
+    email: EmailStr = "genie9718@naver.com" # 받는사람 이메일
+    subject: str = "ace it 문의"
+    message: CreateInquiryRequest
